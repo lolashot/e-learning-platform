6d2ffb0d30aee519bd572ffb5d17af0f5b0502e7
lolashot <ololadeshot@gmail.com>
2022-09-15 21:36:43 +0100

    update lecturer.js

--- hamburger.js
+++ hamburger.js
@@ -2,20 +2,20 @@ window.addEventListener('scroll', function () {
     const header = document.querySelector('header');
     const hamburger = document.querySelector('menuToggle');
     header.classList.toggle("sticky", window.scrollY > 0);
-    hamburger.classList.add("sticky", window.scrollY > 0);
-
 });
 
-function toggleMenu() {
-    const menuToggle = document.querySelector('.menuToggle');
-    const navigation = document.querySelector('.navigation');
-    menuToggle.classList.toggle('active');
-    navigation.classList.toggle('active');
-    const w = window.innerWidth;
-if (window.innerWidth > 767) {
-    navigation.classList.toggle('active');
 
+
+const toggle = document.querySelectorAll('.toggler');
+toggle.forEach((element) => element.addEventListener('click', () => {
+    const menuToggle = document.querySelector('.menuToggle');
+  const navigate = document.querySelector('.navigation');
+  menuToggle.classList.toggle('active');
+  navigate.classList.toggle('active');
+  if (window.innerWidth > 767) {
+    navigate.classList.remove('active');
 }
-}
+
+}));
 
 
--- lecturers.js
+++ lecturers.js
@@ -1,76 +1,88 @@
 const lecturers = [
   {
-    id: '1',
+    id: '0',
     name: 'Joe Praise',
     position: 'chief technical director',
     description: 'a world class professor of computer science',
-    image: './assets/images/lecturer1.jpg'
+    image: 'assets/images/lecturer1.jpg'
   },
   {
-    id: '2',
+    id: '1',
     name: 'Smith John ',
     position: 'react tutor',
     description: 'with over 20yrs experience in coding, and worked at microsoft',
     image: './assets/images/lecturer2.jpg'
   },
   {
-    id: '3',
-    name: '',
+    id: '2',
+    name: 'Grace Craig',
     position: 'javascript tutor',
     image: './assets/images/lecturer1.jpg',
     description: 'with over 20yrs experience in coding, and worked at microsoft',
   },
   {
-    id: '4',
-    name: '',
+    id: '3',
+    name: 'Aron Sher',
     position: 'node js tutor',
     description: 'a world class professor of computer science',
-    images: './assets/images/lecturer3.jpg'
+    image: './assets/images/lecturer3.jpg'
   },
   {
-    id: '5',
-    name: '',
+    id: '4',
+    name: 'Martha Ray',
     position: 'angular tutor',
     description: 'with over 20yrs experience in coding, and worked at microsoft',
     image: './assets/images/lecturer2.jpg'
 
   },
   {
-    id: '6',
-    name: '',
+    id: '5',
+    name: 'Fred Ann',
     position: 'tutor',
     description: 'a world class professor of computer science',
-    image: './assets/images/lecturer1.jpg'
+    image: 'assets/images/lecturer1.jpg'
+
+  },
+  {
+    id: '6',
+    name: 'Glory Yuu',
+    position: 'tutor',
+    description: 'a world class professor of computer science',
+    image: 'assets/images/lecturer3.jpg'
 
   },
   {
     id: '7',
-    name: '',
+    name: 'May Cloe',
     position: 'tutor',
     description: 'a world class professor of computer science',
-    image: './assets/images/lecturer3.jpg'
-
-  },
+    image: 'assets/images/lecturer2.jpg'
+  },    
   {
     id: '8',
-    name: '',
+    name: 'Love Cloe',
     position: 'tutor',
     description: 'a world class professor of computer science',
-    image: './assets/images/lecturer2.jpg'
-
-  }                               
+    image: 'assets/images/lecturer2.jpg'
+  }                                 
 ]
 
 
-document.addEventListener('load', (event) => {
+window.addEventListener('load', () => {
     const nodeList = document.querySelectorAll('.lecturers');
+    console.log('first', nodeList);
+    let found = {};
+
     nodeList.forEach((element) => {
-        console.log('jjj', element.id);
-      const found = lecturers.find((lecturer) => lecturer.id === element.id,
-      console.log('jjj', lecturer.id));
-      document.getElementById('name').innerHTML = found.name;
-      document.getElementById('position').innerHTML = found.position;
-      document.getElementById('description').innerHTML = found.description;
-    
+        console.log('jjj', element.children[0].attributes[0].value);
+
+      found = lecturers.find((lecturer) => lecturer.id == element.id);
+      console.log('222', found.name)
+      if (found) {
+      element.children[0].attributes[0].value   = found.image;
+      element.children[1].children[0].innerHTML = found.name;
+      element.children[1].children[1].innerHTML = found.position;
+      element.children[1].children[2].innerHTML = found.description;
+      }
     });
   });
