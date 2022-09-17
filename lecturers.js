3a5131d27a5e4d7a7660d6886b73c8568099cdce
lolashot <ololadeshot@gmail.com>
2022-09-17 07:23:35 +0100

    review corrections

--- lecturers.js
+++ lecturers.js
@@ -9,28 +9,28 @@ const lecturers = [
   {
     id: '1',
     name: 'Smith John ',
-    position: 'react tutor',
+    position: 'Former ICT director at Microsoft',
     description: 'with over 20yrs experience in coding, and worked at microsoft',
     image: './assets/images/lecturer2.jpg',
   },
   {
     id: '2',
     name: 'Grace Craig',
-    position: 'javascript tutor',
+    position: 'Former Mentor director at Microverse',
     image: './assets/images/lecturer1.jpg',
     description: 'with over 20yrs experience in coding, and worked at microsoft',
   },
   {
     id: '3',
     name: 'Aron Sher',
-    position: 'node js tutor',
+    position: 'node js code affiliator',
     description: 'a world class professor of computer science',
     image: './assets/images/lecturer3.jpg',
   },
   {
     id: '4',
     name: 'Martha Ray',
-    position: 'angular tutor',
+    position: 'angular tutor, partner and contributor',
     description: 'with over 20yrs experience in coding, and worked at microsoft',
     image: './assets/images/lecturer2.jpg',
 
@@ -38,7 +38,7 @@ const lecturers = [
   {
     id: '5',
     name: 'Fred Ann',
-    position: 'tutor',
+    position: 'Former Venture Capitalist director at LinkedIn',
     description: 'a world class professor of computer science',
     image: 'assets/images/lecturer1.jpg',
 
@@ -46,7 +46,7 @@ const lecturers = [
   {
     id: '6',
     name: 'Glory Yuu',
-    position: 'tutor',
+    position: 'Former Senior Html Tutor at MSI',
     description: 'a world class professor of computer science',
     image: 'assets/images/lecturer3.jpg',
 
@@ -54,15 +54,8 @@ const lecturers = [
   {
     id: '7',
     name: 'May Cloe',
-    position: 'tutor',
-    description: 'a world class professor of computer science',
-    image: 'assets/images/lecturer2.jpg',
-  },
-  {
-    id: '8',
-    name: 'Love Cloe',
-    position: 'tutor',
-    description: 'a world class professor of computer science',
+    position: 'Facebook Senior Research Fellow',
+    description: 'a world class professor of reseach and development',
     image: 'assets/images/lecturer2.jpg',
   },
 ];
@@ -76,7 +69,7 @@ window.addEventListener('load', () => {
       element.children[0].attributes[0].value = found.image;
       element.children[1].children[0].innerHTML = found.name;
       element.children[1].children[1].innerHTML = found.position;
-      element.children[1].children[2].innerHTML = found.description;
+      element.children[1].children[3].innerHTML = found.description;
     }
   });
 });
